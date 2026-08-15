31a3346acbdc9d83ba97bccb755aac28608d259a
shama <shama.parween6204@gmail.com>
2026-08-16 00:37:46 +0530

    Initial commit: Spring Boot setup with MySQL connection

---
+++ src/main/java/com/roommate/roommate_backend/RoommateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.roommate.roommate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoommateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoommateBackendApplication.class, args);
+	}
+
+}
